c8bfd4a5f31ba598242c407ff00ad0fef9ded90f
vladwulf <agaev.vladimir@gmail.com>
2022-06-14 18:33:25 +0100

    chore: add gradient border

--- components/swap/parts/ChainSwitcher.tsx
+++ components/swap/parts/ChainSwitcher.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 export const ChainSwitcher = () => {
   return (
-    <div className="border border-[#008cfe] bg-gradient-to-b from-[#02323c] to-[#031f3d] h-10 w-10 rounded-2xl p-2.5">
-      <div className="relative h-full w-full">
-        <Image layout="fill" src="/assets/ui/double-arrows.svg" />
+    <div className="bg-gradient-to-b from-[#00fbfb] to-[#0066ff] h-12 w-12 p-[1px] rounded-2xl cursor-pointer">
+      <div className="flex justify-center items-center h-full w-full bg-gradient-to-b from-[#00343d] to-[#001f3f] rounded-2xl p-3">
+        <div className="relative w-full h-full">
+          <Image layout="fill" src="/assets/ui/double-arrows.svg" />
+        </div>
       </div>
     </div>
   );
