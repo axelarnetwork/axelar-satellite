a7d9fa67397a7c81677136c79f55a883b84e495b
vladwulf <agaev.vladimir@gmail.com>
2022-06-20 13:25:52 +0100

    chore: fix logic

--- components/swap/parts/DestChainSelector.tsx
+++ components/swap/parts/DestChainSelector.tsx
@@ -1,39 +1,71 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { allChains } from "../../../config/chains";
 import { useSwapStore } from "../../../store";
 import { useOnClickOutside } from "usehooks-ts";
+import { convertChainName } from "../../../utils/transformers";
 
 export const DestChainSelector = () => {
+  const [imgError, setImgError] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const { destChain, setDestChain } = useSwapStore((state) => state);
-
   const ref = useRef(null);
+
+  useEffect(() => {
+    setImgError(false);
+  }, [destChain]);
+
   useOnClickOutside(ref, () => {
     dropdownOpen && handleOnDropdownToggle();
   });
-
-  function handleOnChainChange(chain: string) {
-    setDestChain(chain);
-  }
 
   function handleOnDropdownToggle() {
     setDropdownOpen(!dropdownOpen);
   }
 
+  function getChainImg() {
+    if (imgError) return "/assets/chains/default.logo.svg";
+    return `/assets/chains/${destChain.chainInfo.chainSymbol.toLowerCase()}.logo.svg`;
+  }
+
+  function renderChainDropdown() {
+    if (!dropdownOpen) return null;
+
+    return (
+      <ul
+        tabIndex={0}
+        className="p-2 rounded-lg shadow dropdown-content menu bg-[#02141b] left-0 w-full h-64 overflow-auto"
+      >
+        {allChains.map((chain) => {
+          return (
+            <li key={chain.chainInfo.chainSymbol}>
+              <button onClick={() => setDestChain(chain)}>
+                {chain.chainInfo.chainName}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
   return (
     <div>
       <label className="block text-xs">To</label>
-      <div className="static dropdown">
+      <div
+        className="static dropdown dropdown-open"
+        onClick={handleOnDropdownToggle}
+      >
         <div tabIndex={0}>
           <div className="flex items-center space-x-2 text-lg font-medium cursor-pointer">
             <Image
-              src="/assets/chains/cosmos.logo.svg"
+              src={getChainImg()}
               layout="intrinsic"
               width={40}
               height={40}
+              onError={() => setImgError(true)}
             />
-            <span>{destChain}</span>
+            <span>{convertChainName(destChain.chainInfo.chainName)}</span>
             <div className="flex items-center">
               <Image
                 src="/assets/ui/arrow-down.svg"
@@ -44,22 +76,7 @@ export const DestChainSelector = () => {
             </div>
           </div>
         </div>
-        <ul
-          tabIndex={0}
-          className="p-2 rounded-lg shadow dropdown-content menu bg-[#02141b] left-0 w-full h-64 overflow-auto"
-        >
-          {allChains.map((chain) => {
-            return (
-              <li key={chain.chainInfo.chainName}>
-                <button
-                  onClick={() => handleOnChainChange(chain.chainInfo.chainName)}
-                >
-                  {chain.chainInfo.chainName}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+        {renderChainDropdown()}
       </div>
     </div>
   );
