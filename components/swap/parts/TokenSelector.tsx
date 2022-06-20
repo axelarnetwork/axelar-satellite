fea449ff24d8b33f4aba83f6a487dc3723026830
vladwulf <agaev.vladimir@gmail.com>
2022-06-20 17:52:42 +0100

    chore: add token selector dropdown logic

--- components/swap/parts/TokenSelector.tsx
+++ components/swap/parts/TokenSelector.tsx
@@ -1,26 +1,103 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Image from "next/image";
+import { useSwapStore } from "../../../store";
+import { useOnClickOutside } from "usehooks-ts";
+import { ENVIRONMENT } from "../../../config/constants";
+
+const defaultAssetImg = "/assets/tokens/default.logo.svg";
 
 export const TokenSelector = () => {
+  const [imgError, setImgError] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const { asset, selectableAssetList, setAsset, srcChain } = useSwapStore(
+    (state) => state
+  );
+  const ref = useRef(null);
+
+  useOnClickOutside(ref, () => {
+    dropdownOpen && handleOnDropdownToggle();
+  });
+
+  function handleOnDropdownToggle() {
+    setDropdownOpen(!dropdownOpen);
+  }
+
+  function renderAssetDropdown() {
+    if (!dropdownOpen) return null;
+
+    return (
+      <ul
+        tabIndex={0}
+        className="p-2 rounded-lg shadow dropdown-content menu bg-[#02141b] left-0 w-full h-64 overflow-auto"
+      >
+        {selectableAssetList.map((asset) => {
+          return (
+            <li key={asset.common_key[ENVIRONMENT]}>
+              <button onClick={() => setAsset(asset)}>
+                <Image
+                  src={`/assets/tokens/${asset.common_key[
+                    ENVIRONMENT
+                  ].toLowerCase()}.logo.svg`}
+                  layout="intrinsic"
+                  width={40}
+                  height={40}
+                  onError={(e) => {
+                    e.currentTarget.src = defaultAssetImg;
+                    e.currentTarget.srcset = defaultAssetImg;
+                  }}
+                />
+                <span>
+                  {
+                    asset.chain_aliases[
+                      srcChain.chainInfo.chainName.toLowerCase()
+                    ]?.assetName
+                  }
+                </span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+
+  function renderAssetName() {
+    if (!asset) return "Select an asset";
+    return asset.chain_aliases[srcChain.chainInfo.chainName.toLowerCase()]
+      ?.assetName;
+  }
+
   return (
-    <div>
-      <label className="text-xs">I want to transfer</label>
-      <div className="text-lg font-medium flex items-center space-x-2">
-        <Image
-          src="/assets/tokens/axl-dai.logo.svg"
-          layout="intrinsic"
-          width={40}
-          height={40}
-        />
-        <span>axlDai</span>
-        <div className="flex items-center">
-          <Image
-            src="/assets/ui/arrow-down.svg"
-            layout="intrinsic"
-            width={25}
-            height={25}
-          />
+    <div ref={ref}>
+      <label className="block text-xs">I want to transfer</label>
+      <div
+        className="static dropdown dropdown-open"
+        onClick={handleOnDropdownToggle}
+      >
+        <div tabIndex={0}>
+          <div className="flex items-center space-x-2 text-lg font-medium cursor-pointer">
+            <Image
+              src={`/assets/tokens/${asset?.common_key[ENVIRONMENT]}.logo.svg`}
+              layout="intrinsic"
+              width={40}
+              height={40}
+              onError={(e) => {
+                e.currentTarget.src = defaultAssetImg;
+                e.currentTarget.srcset = defaultAssetImg;
+              }}
+            />
+            <span>{renderAssetName()}</span>
+            <div className="flex items-center">
+              <Image
+                src="/assets/ui/arrow-down.svg"
+                layout="intrinsic"
+                width={25}
+                height={25}
+              />
+            </div>
+          </div>
         </div>
+        {renderAssetDropdown()}
       </div>
     </div>
   );
