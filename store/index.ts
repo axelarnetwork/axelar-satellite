e9691d5d5cfc8e7931a7814f8e2775320cef3315
vladwulf <agaev.vladimir@gmail.com>
2022-06-20 13:19:09 +0100

    chore: enable devtools + update store

--- store/index.ts
+++ store/index.ts
@@ -1,22 +1,26 @@
+import { ChainInfo, Chain } from "@axelar-network/axelarjs-sdk";
 import create from "zustand";
 import { devtools, persist } from "zustand/middleware";
+import { allChains } from "../config/chains";
 
 interface SwapSwate {
-  srcChain: any;
-  destChain: any;
+  srcChain: Chain;
+  destChain: Chain;
   setSrcChain: (chain: any) => void;
   setDestChain: (chain: any) => void;
 }
 
-export const useSwapStore = create<SwapSwate>()((set) => ({
-  srcChain: "Ethereum",
-  destChain: "Cosmos",
-  setSrcChain: (chain: string) =>
-    set({
-      srcChain: chain,
-    }),
-  setDestChain: (chain: string) =>
-    set({
-      destChain: chain,
-    }),
-}));
+export const useSwapStore = create<SwapSwate>()(
+  devtools((set) => ({
+    srcChain: allChains[0],
+    destChain: allChains[1],
+    setSrcChain: (chain: Chain) =>
+      set({
+        srcChain: chain,
+      }),
+    setDestChain: (chain: Chain) =>
+      set({
+        destChain: chain,
+      }),
+  }))
+);
