feed607dc5fb795cb491de6cf1f392515af981e7
vladwulf <agaev.vladimir@gmail.com>
2022-08-02 13:48:43 +0100

    chore: adjust navbar

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <div className="fixed flex items-center w-full h-24">
+    <div className="fixed flex items-center w-full h-36">
       <nav className="flex items-center w-full max-w-screen-xl px-4 mx-auto">
         <div className="flex">
           <Image
@@ -14,11 +14,12 @@ export const Navbar = () => {
           />
           <div className="ml-3">
             <div className="text-2xl font-bold">Satellite</div>
-            <div className="text-lg font-normal">(Beta) Powered by Axelar</div>
+            <div className="text-sm font-normal">(Beta) Powered by Axelar</div>
           </div>
         </div>
         <div className="flex items-center ml-auto">
-          <label className="swap swap-rotate">
+          <input type="checkbox" className="toggle toggle-primary" />
+          {/* <label className="swap swap-rotate">
             <input type="checkbox" />
 
             <svg
@@ -36,7 +37,7 @@ export const Navbar = () => {
             >
               <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
             </svg>
-          </label>
+          </label> */}
         </div>
       </nav>
     </div>
