9229e2430c15f1e388a5cfd16a9867403bdbdade
vladwulf <agaev.vladimir@gmail.com>
2022-08-17 11:49:04 +0100

    fix: revert network name

--- config/web3/evm/chains.testnet.ts
+++ config/web3/evm/chains.testnet.ts
@@ -3,7 +3,7 @@ import { Chain } from "wagmi";
 const ethereum: Chain = {
   id: 3,
   name: "Ropsten Testnet",
-  network: "ethereum",
+  network: "ropsten",
   nativeCurrency: {
     name: "Ether",
     symbol: "ETH",
