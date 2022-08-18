e56ab1b81a0c7c6f35c336c8c1022d9d39941809
vladwulf <agaev.vladimir@gmail.com>
2022-08-18 16:48:02 +0100

    chore: change layout based on suggestions

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,9 @@ const Home: NextPage = () => {
 
       <Layout>
         <VideoBackground />
-        <div className="h-full grid grid-cols-1 pt-[25vh] lg:grid-cols-2 justify-items-center lg:justify-items-stretch gap-10">
-          <PageHeader />
-          <div className="flex items-start justify-end">
+        <div className="h-full grid grid-cols-1 pt-[25vh] lg:grid-cols-1 justify-items-center lg:justify-items-stretch gap-10">
+          {/* <PageHeader /> */}
+          <div className="flex items-start justify-center">
             {storeReady && <SwapBox />}
           </div>
         </div>
