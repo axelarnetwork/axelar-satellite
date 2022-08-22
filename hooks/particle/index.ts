2322c2a3aae8562dabd0dfd86dcf3403c53fd982
vladwulf <agaev.vladimir@gmail.com>
2022-08-22 16:36:44 +0100

    chore: reduce particle speed

--- hooks/particle/index.ts
+++ hooks/particle/index.ts
@@ -16,7 +16,7 @@ export const drawBackground = () => {
       let angle = (Math.PI / 2) * (Math.random() - 0.5) * 2;
       particle.x = Math.random() * width;
       particle.y = Math.random() * height;
-      particle.life = Math.random() * 1000 + 1000;
+      particle.life = Math.random() * 1000 + 10000;
       particle.size = Math.random() * 1.5;
       particle.maxLife = particle.life;
       system.addParticle(particle);
@@ -28,13 +28,13 @@ export const drawBackground = () => {
         particle.y = Math.random() * height;
         particle.vx = 0;
         particle.vy = 0;
-        particle.life = Math.random() * 1000 + 1000;
+        particle.life = Math.random() * 1000 + 10000;
         particle.maxLife = particle.life;
       }
 
       particle.life -= 10;
-      particle.accelX = (Math.random() - 0.5) * 0.02;
-      particle.accelY = (Math.random() - 0.5) * 0.02;
+      particle.accelX = (Math.random() - 0.5) * 0.002;
+      particle.accelY = (Math.random() - 0.5) * 0.002;
 
       if (particle.life >= particle.maxLife / 2) {
         particle.alpha = 1 - particle.life / particle.maxLife;
