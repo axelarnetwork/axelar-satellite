d214e7fd0b6b602109aae62212f427492c34dd6c
vladwulf <agaev.vladimir@gmail.com>
2022-08-22 18:39:48 +0100

    chore: add more messaging

--- components/swap/states/WaitEvmConfirmationState.tsx
+++ components/swap/states/WaitEvmConfirmationState.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { SpinnerCircular } from "spinners-react";
+import Image from "next/image";
+
+import { SpinnerCircular, SpinnerRoundOutlined } from "spinners-react";
 import { erc20ABI, useContractEvent } from "wagmi";
 import { ENVIRONMENT } from "../../../config/constants";
 import { getDestChainId, useSwapStore } from "../../../store";
+import { copyToClipboard } from "../../../utils";
 import { SwapStatus } from "../../../utils/enums";
-import { InputWrapper } from "../../common";
+import { AddressShortener, InputWrapper } from "../../common";
 import { ProgressBar } from "./parts";
 
 export const WaitEvmConfirmationState = () => {
@@ -16,6 +19,7 @@ export const WaitEvmConfirmationState = () => {
     setSwapStatus,
     txInfo,
     setTxInfo,
+    depositAddress,
   } = useSwapStore((state) => state);
 
   const chainAlias = destChain.chainIdentifier[ENVIRONMENT];
@@ -41,34 +45,59 @@ export const WaitEvmConfirmationState = () => {
 
   function renderConfirmations() {
     return (
-      <div className="flex items-center gap-x-2">
-        <SpinnerCircular
-          size={20}
-          thickness={147}
-          color={"#00a5ff"}
-          secondaryColor={"#375f73"}
-        />
-        <div className="flex flex-col text-center">
-          <span className="text-sm">
-            Transfer on {srcChain.chainName} detected!
-          </span>
-          <span className="text-xs font-light text-gray-200">
-            Transfering your {asset?.common_key[ENVIRONMENT]} to{" "}
-            {destChain.chainName}...
-          </span>
+      <div className="flex flex-col justify-center h-full text-center gap-y-1">
+        <div>
+          <h4 className="text-sm">Deposit Address</h4>
+          <div className="flex justify-center text-sm font-bold text-info gap-x-2">
+            <AddressShortener value={depositAddress} />
+            <div
+              className="cursor-pointer"
+              onClick={() => copyToClipboard(depositAddress)}
+            >
+              <Image src={"/assets/ui/copy.svg"} height={16} width={16} />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h4 className="text-sm">Transaction Hash</h4>
+          <div className="flex justify-center text-sm font-bold text-info gap-x-2">
+            <AddressShortener value={txInfo.sourceTxHash} />
+            <div
+              className="cursor-pointer"
+              onClick={() => copyToClipboard(txInfo.sourceTxHash as string)}
+            >
+              <Image src={"/assets/ui/copy.svg"} height={16} width={16} />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center gap-x-2">
+          <SpinnerRoundOutlined
+            className="text-blue-500"
+            size={20}
+            color="#00a6ff"
+          />
+          <div className="flex flex-col text-center">
+            <span className="text-base">
+              Transfer on {srcChain.chainName} detected!
+            </span>
+            <span className="text-base font-light text-gray-200">
+              Transfering your {asset?.common_key[ENVIRONMENT]} to{" "}
+              {destChain.chainName}...
+            </span>
+          </div>
         </div>
       </div>
     );
   }
 
   return (
-    <InputWrapper className="h-40">
+    <InputWrapper className="h-72">
       <div className="h-full space-x-2">
         <div className="flex flex-col w-full h-full">
-          <div className="h-full">
+          <div className="relative flex flex-col h-full">
             <ProgressBar level={2} />
 
-            <div className="flex items-center justify-center mt-6 text-xs gap-x-2">
+            <div className="flex items-center justify-center h-full mt-auto text-xs gap-x-2">
               {renderConfirmations()}
             </div>
           </div>
