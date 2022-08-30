d8a0d31dd0ad75728617a4087eb7f1e632a42587
vladwulf <agaev.vladimir@gmail.com>
2022-08-30 19:21:32 +0100

    clean

--- components/swap/parts/ActionButton.tsx
+++ components/swap/parts/ActionButton.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { useSwapStore, useWalletStore } from "../../../store";
+import { useSwapStore } from "../../../store";
 import { SwapStatus } from "../../../utils/enums";
-import { ConnectButton } from "./ConnectButton";
 import { GenerateDepositAddressButton } from "./GenerateDepositAddressButton";
 
 export const ActionButton = () => {
   const { swapStatus, resetState } = useSwapStore((state) => state);
-  // const wagmiConnected = useWalletStore((state) => state.wagmiConnected);
-  // if (!wagmiConnected) return <ConnectButton />;
   if (swapStatus === SwapStatus.IDLE) return <GenerateDepositAddressButton />;
   if (swapStatus === SwapStatus.GEN_DEPOSIT_ADDRESS)
     return (
