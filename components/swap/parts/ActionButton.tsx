67db5c2c052427ec1c5688f7d584657fd7c2adc5
vladwulf <agaev.vladimir@gmail.com>
2022-08-30 19:21:17 +0100

    chore: update btn styles

--- components/swap/parts/ActionButton.tsx
+++ components/swap/parts/ActionButton.tsx
@@ -11,7 +11,7 @@ export const ActionButton = () => {
   if (swapStatus === SwapStatus.IDLE) return <GenerateDepositAddressButton />;
   if (swapStatus === SwapStatus.GEN_DEPOSIT_ADDRESS)
     return (
-      <button className="w-full btn btn-primary" disabled>
+      <button className="w-full btn btn-primary cursor-not-allowedy">
         <div className="flex items-center gap-3">
           <span>Processing...</span>
         </div>
@@ -19,7 +19,7 @@ export const ActionButton = () => {
     );
   if (swapStatus === SwapStatus.WAIT_FOR_DEPOSIT)
     return (
-      <button className="w-full btn btn-primary" disabled>
+      <button className="w-full cursor-not-allowed btn btn-primary">
         <div className="flex items-center gap-3">
           <span>Waiting for deposit...</span>
         </div>
@@ -27,7 +27,7 @@ export const ActionButton = () => {
     );
   if (swapStatus === SwapStatus.WAIT_FOR_CONFIRMATION)
     return (
-      <button className="w-full btn btn-primary" disabled>
+      <button className="w-full cursor-not-allowed btn btn-primary">
         <div className="flex items-center gap-3">
           <span>Waiting for confirmation...</span>
         </div>
