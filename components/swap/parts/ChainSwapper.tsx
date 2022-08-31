7f5b1beed85522e7d282d5250c1162b24d7d57b9
Canh Trinh <canh@axelar.network>
2022-08-31 01:21:08 -0400

    chore: update query params on switch chains #46

--- components/swap/parts/ChainSwapper.tsx
+++ components/swap/parts/ChainSwapper.tsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import { useSwapStore } from "../../../store";
+import { useRouter } from "next/router";
 
 export const ChainSwapper = () => {
-  const switchChains = useSwapStore((state) => state.switchChains);
+  const { switchChains, srcChain, destChain } = useSwapStore((state) => state);
+  const router = useRouter();
+
+  const updateQueryParamsAndSwitch = () => {
+    switchChains();
+    router.push({
+      query: {
+        ...router.query,
+        source: destChain.chainName.toLowerCase(),
+        destination: srcChain.chainName.toLowerCase(),
+      },
+    });
+  };
+
   return (
     <div
-      onClick={switchChains}
+      onClick={updateQueryParamsAndSwitch}
       className="bg-gradient-to-b from-[#00fbfb] to-[#0066ff] h-10 w-10 p-[1px] rounded-xl cursor-pointer"
     >
       <div className="flex justify-center items-center h-full w-full bg-gradient-to-b from-[#00343d] to-[#001f3f] rounded-xl p-2.5">
