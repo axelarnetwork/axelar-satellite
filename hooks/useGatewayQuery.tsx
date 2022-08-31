8c0c2f85fecf23c4f50c3c9dffa9e39ae78577df
Canh Trinh <canh@axelar.network>
2022-08-30 21:33:57 -0400

    chore: fixing formatting for max transfer amount

--- hooks/useGatewayQuery.tsx
+++ hooks/useGatewayQuery.tsx
@@ -1,10 +1,11 @@
 import { useContractRead } from "wagmi";
 import gatewayABI from "../data/abi/axelarGateway.json";
 import { useSwapStore } from "../store";
-import { formatUnits } from "ethers/lib/utils";
+import { commify, formatUnits } from "ethers/lib/utils";
 import { useAxelarRPCQuery } from "./api/useAxelarRPCQuery";
 import { useEffect, useState } from "react";
 import { getWagmiChains } from "../config/web3";
+import { BigNumber } from "ethers";
 
 const UseGatewayQuery = () => {
   const { asset, destChain } = useSwapStore((state) => state);
@@ -37,10 +38,11 @@ const UseGatewayQuery = () => {
     maxTransferAmount,
     jsx: maxTransferAmount ? (
       <div>
-        {formatUnits(
-          maxTransferAmount,
-          asset?.chain_aliases[destChain?.chainName.toLowerCase()]?.decimals
-        )}
+        {commify(formatUnits(
+          BigNumber.from(maxTransferAmount).div(4),
+          asset?.decimals,
+        ))}{" "}
+        {asset?.chain_aliases[destChain?.chainName.toLowerCase()].assetSymbol}
       </div>
     ) : null,
   };
