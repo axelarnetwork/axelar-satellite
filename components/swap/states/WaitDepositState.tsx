a074783a3f80e873de1c636706d6a6decc12f6af
vladwulf <agaev.vladimir@gmail.com>
2022-09-01 16:45:16 +0100

    chore: add stats to steps 2 & 3

--- components/swap/states/WaitDepositState.tsx
+++ components/swap/states/WaitDepositState.tsx
@@ -10,6 +10,7 @@ import { CosmosWalletTransfer, EvmWalletTransfer, ProgressBar } from "./parts";
 import { copyToClipboard } from "../../../utils";
 
 import { convertChainName } from "../../../utils/transformers";
+import { InitialStats } from "../parts";
 
 export const WaitDepositState = () => {
   const { depositAddress, destAddress, srcChain } = useSwapStore(
@@ -44,38 +45,45 @@ export const WaitDepositState = () => {
   }
 
   return (
-    <InputWrapper className="h-auto">
-      <div className="h-full space-x-2">
-        <div className="flex flex-col w-full h-full">
-          <div className="h-full">
-            <ProgressBar level={2} />
+    <>
+      <InitialStats />
+      <InputWrapper className="h-auto">
+        <div className="h-full space-x-2">
+          <div className="flex flex-col w-full h-full">
+            <div className="h-full">
+              <ProgressBar level={2} />
 
-            <div className="flex items-center justify-center mt-6 text-sm gap-x-2">
-              <div>
-                <label className="block text-center">
-                  {renderTransferInfo()}
-                </label>
-                <div className="flex justify-center font-bold text-info gap-x-2">
-                  <AddressShortener value={depositAddress} />
-                  <div
-                    className="cursor-pointer"
-                    onClick={() => copyToClipboard(depositAddress)}
-                  >
-                    <Image src={"/assets/ui/copy.svg"} height={16} width={16} />
+              <div className="flex items-center justify-center mt-6 text-sm gap-x-2">
+                <div>
+                  <label className="block text-center">
+                    {renderTransferInfo()}
+                  </label>
+                  <div className="flex justify-center font-bold text-info gap-x-2">
+                    <AddressShortener value={depositAddress} />
+                    <div
+                      className="cursor-pointer"
+                      onClick={() => copyToClipboard(depositAddress)}
+                    >
+                      <Image
+                        src={"/assets/ui/copy.svg"}
+                        height={16}
+                        width={16}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
+              {renderWalletSection()}
             </div>
-            {renderWalletSection()}
-          </div>
-          <div className="w-full mt-auto">
-            <div className="my-0 divider" />
-            <div className="w-full text-xs font-medium text-center text-gray-500">
-              Your destination address {destAddress}
+            <div className="w-full mt-auto">
+              <div className="my-0 divider" />
+              <div className="w-full text-xs font-medium text-center text-gray-500">
+                Your destination address {destAddress}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </InputWrapper>
+      </InputWrapper>
+    </>
   );
 };
