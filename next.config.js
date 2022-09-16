d2f37d2384f37dcc912e3508dfc6185e585c3abe
vladwulf <agaev.vladimir@gmail.com>
2022-09-16 20:08:01 +0100

    chore: revert security headers

--- next.config.js
+++ next.config.js
@@ -3,29 +3,14 @@ const { createSecureHeaders } = require("next-secure-headers");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/(.*)",
-  //       headers: createSecureHeaders({
-  //         contentSecurityPolicy: {
-  //           directives: {
-  //             "default-src": [
-  //               "self",
-  //               "axelar-testnet.s3.us-east-2.amazonaws.com",
-  //               "axelar-mainnet.s3.us-east-2.amazonaws.com",
-  //             ],
-  //             "connect-src": [
-  //               "self",
-  //               "axelar-testnet.s3.us-east-2.amazonaws.com",
-  //               "axelar-mainnet.s3.us-east-2.amazonaws.com",
-  //             ],
-  //           },
-  //         },
-  //       }),
-  //     },
-  //   ];
-  // },
+  headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders(),
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
