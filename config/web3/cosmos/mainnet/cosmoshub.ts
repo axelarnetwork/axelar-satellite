ea1625bfffa62d53ff4710ea5ccdb6157e269bcf
vladwulf <agaev.vladimir@gmail.com>
2022-11-09 16:45:20 +0000

    chore: update cosmoshub rpc

--- config/web3/cosmos/mainnet/cosmoshub.ts
+++ config/web3/cosmos/mainnet/cosmoshub.ts
@@ -2,9 +2,11 @@ import { CosmosChain } from "../interface";
 
 export default {
   rest: "https://api.cosmos.network",
-  rpc: "https://cosmoshub-4--rpc--full.datahub.figment.io/apikey/6d8baa3d3e97e427db4bd7ffcfb21be4",
+  rpc: "https://mainnet-rpc-router.axelar-dev.workers.dev?chain=cosmoshub",
   chainId: "cosmoshub-4",
   chainIdentifier: "cosmoshub",
-  currencies: [{ coinDenom: "ATOM", coinMinimalDenom: "uatom", coinDecimals: 6 }],
-  chainToAxelarChannelId: "channel-293"
+  currencies: [
+    { coinDenom: "ATOM", coinMinimalDenom: "uatom", coinDecimals: 6 },
+  ],
+  chainToAxelarChannelId: "channel-293",
 } as CosmosChain;
