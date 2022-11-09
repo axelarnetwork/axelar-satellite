049e81649436affbdec8792b4b359568d1c11e9b
Vladimir Agaev <agaev.vladimir@gmail.com>
2022-11-09 21:17:03 +0000

    chore: update evmos config (#180)
    * chore: update evmos config
    
    * fix: public lcd

--- config/web3/cosmos/mainnet/evmos.ts
+++ config/web3/cosmos/mainnet/evmos.ts
@@ -2,8 +2,8 @@ import { Bech32Address } from "@keplr-wallet/cosmos";
 import { CosmosChain } from "../interface";
 
 export default {
-  rpc: "https://tendermint.bd.evmos.org:26657",
-  rest: "https://rest.bd.evmos.org:1317", //"https://mainnet-lcd-router.axelar-dev.workers.dev", TODO - get LCD router to work so that we can retry
+  rpc: "https://mainnet-rpc-router.axelar-dev.workers.dev/?chain=evmos",
+  rest: "https://evmos-rest.publicnode.com", //"https://mainnet-lcd-router.axelar-dev.workers.dev", TODO - get LCD router to work so that we can retry
   chainId: "evmos_9001-2",
   chainName: "Evmos",
   stakeCurrency: {
