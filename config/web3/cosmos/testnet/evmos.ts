354895ab8d71d2e8a2aacfa171aac8f6c6ae6899
vladwulf <agaev.vladimir@gmail.com>
2022-11-10 20:28:38 +0000

    format

--- config/web3/cosmos/testnet/evmos.ts
+++ config/web3/cosmos/testnet/evmos.ts
@@ -2,48 +2,48 @@ import { CosmosChain } from "../interface";
 
 export default {
   rpc: "https://testnet-rpc-router.axelar-dev.workers.dev/?chain=evmos",
-  "rest": "https://rest.bd.evmos.dev:1317",
-  "chainId": "evmos_9000-4",
-  "chainName": "Evmos Testnet",
-  "stakeCurrency": {
-    "coinDenom": "EVMOS",
-    "coinMinimalDenom": "atevmos",
-    "coinDecimals": 18,
-    "coinGeckoId": "evmos"
+  rest: "https://rest.bd.evmos.dev:1317",
+  chainId: "evmos_9000-4",
+  chainName: "Evmos Testnet",
+  stakeCurrency: {
+    coinDenom: "EVMOS",
+    coinMinimalDenom: "atevmos",
+    coinDecimals: 18,
+    coinGeckoId: "evmos",
   },
-  "bech32Config": {
-    "bech32PrefixAccAddr": "evmos",
-    "bech32PrefixAccPub": "evmospub",
-    "bech32PrefixValAddr": "evmosvaloper",
-    "bech32PrefixValPub": "evmosvaloperpub",
-    "bech32PrefixConsAddr": "evmosvalcons",
-    "bech32PrefixConsPub": "evmosvalconspub"
+  bech32Config: {
+    bech32PrefixAccAddr: "evmos",
+    bech32PrefixAccPub: "evmospub",
+    bech32PrefixValAddr: "evmosvaloper",
+    bech32PrefixValPub: "evmosvaloperpub",
+    bech32PrefixConsAddr: "evmosvalcons",
+    bech32PrefixConsPub: "evmosvalconspub",
   },
-  "bip44": {
-    "coinType": 118
+  bip44: {
+    coinType: 118,
   },
-  "currencies": [
+  currencies: [
     {
-      "coinDenom": "EVMOS",
-      "coinMinimalDenom": "atevmos",
-      "coinDecimals": 18,
-      "coinGeckoId": "evmos"
-    }
+      coinDenom: "EVMOS",
+      coinMinimalDenom: "atevmos",
+      coinDecimals: 18,
+      coinGeckoId: "evmos",
+    },
   ],
-  "feeCurrencies": [
+  feeCurrencies: [
     {
-      "coinDenom": "EVMOS",
-      "coinMinimalDenom": "atevmos",
-      "coinDecimals": 18,
-      "coinGeckoId": "evmos"
-    }
+      coinDenom: "EVMOS",
+      coinMinimalDenom: "atevmos",
+      coinDecimals: 18,
+      coinGeckoId: "evmos",
+    },
   ],
-  "gasPriceStep": {
-    "low": 25000000000,
-    "average": 25000000000,
-    "high": 40000000000
+  gasPriceStep: {
+    low: 25000000000,
+    average: 25000000000,
+    high: 40000000000,
   },
-  "features": ["stargate", "no-legacy-stdTx", "ibc-transfer"],
+  features: ["stargate", "no-legacy-stdTx", "ibc-transfer"],
   chainIdentifier: "evmos",
-  chainToAxelarChannelId: "channel-22"
+  chainToAxelarChannelId: "channel-22",
 } as CosmosChain;
