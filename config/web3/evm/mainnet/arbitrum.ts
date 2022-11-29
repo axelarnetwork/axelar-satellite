ef759636ac8ef9a66c2e58eaa7182aeffb85d2c6
Canh Trinh <canh@axelar.network>
2022-11-29 09:43:15 -0500

    chore: adding celo and arbitrum wallet configs

---
+++ config/web3/evm/mainnet/arbitrum.ts
@@ -0,0 +1,22 @@
+export default {
+    id: 42_161,
+    name: "Arbitrum",
+    network: "ethereum",
+    networkNameOverride: "arbitrum",
+    nativeCurrency: {
+      name: "ETH",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    blockExplorers: {
+      default: {
+        name: "Arbitrum Explorer",
+        url: "https://arbiscan.io/",
+      },
+    },
+    rpcUrls: {
+      default: "https://arb1.arbitrum.io/rpc",
+    },
+    testnet: false,
+  };
+  
--- config/web3/evm/mainnet/index.ts
+++ config/web3/evm/mainnet/index.ts
@@ -5,10 +5,14 @@ import avalanche from "./avalanche";
 import fantom from "./fantom";
 import moonbeam from "./moonbeam";
 import polygon from "./polygon";
+import arbitrum from "./arbitrum";
+import celo from "./celo";
 
 export const mainnetChains: ChainExtension[] = [
+  arbitrum,
   avalanche,
   binance,
+  celo,
   ethereum,
   moonbeam,
   fantom,
