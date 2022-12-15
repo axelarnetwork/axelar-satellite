99613922efae787a1c4e3c5811ede70bd843bd9c
vladwulf <agaev.vladimir@gmail.com>
2022-12-15 16:13:18 +0000

    add better type for asset config

--- types/index.ts
+++ types/index.ts
@@ -5,3 +5,29 @@ export type RouteQuery = {
 };
 
 export type Hash = `0x${string}`;
+
+export interface AssetConfigExtended {
+  id: string;
+  common_key: {
+    devnet: string;
+    testnet: string;
+    mainnet: string;
+  };
+  native_chain: string;
+  fully_supported: boolean;
+  decimals: number;
+  wrapped_erc20: string;
+  is_gas_token: boolean;
+  chain_aliases: Record<
+    string,
+    {
+      assetSymbol: string;
+      assetName: string;
+      minDepositAmt: number;
+      ibcDenom: string;
+      fullDenomPath: string;
+      tokenAddress: string;
+      mintLimit: number;
+    }
+  >;
+}
