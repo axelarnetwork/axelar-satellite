5455601b89266982ea75c5d2438d0476864f8428
vladwulf <agaev.vladimir@gmail.com>
2023-01-19 19:06:27 +0000

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { NextPage } from "next";
+import Link from "next/link";
+
+const NOT_FOUND: NextPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <Link href="/">
+        <a>Go back home</a>
+      </Link>
+    </div>
+  );
+};
+
+export default NOT_FOUND;
