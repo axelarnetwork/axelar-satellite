7bc88c70e63858c1c3eef95bc132b198019d9840
Canh Trinh <canh@axelar.network>
2023-01-24 22:06:39 -0500

    chore: cleanup selection logic

--- components/swap/parts/DestinationTokenSelector.tsx
+++ components/swap/parts/DestinationTokenSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 import { AssetInfo } from "@axelar-network/axelarjs-sdk";
@@ -37,7 +37,7 @@ export const DestinationTokenSelector = ({
   const [selectedAssetSymbol, setSelectedAssetSymbol] = useState(
     shouldUnwrapAsset ? unwrappedAssetSymbol : wrappedAssetSymbol
   );
-  const squidTokens = useSquidStateStore((state) => state.squidTokens);
+  const [selectedSquid, setSelectedSquid] = useState<AssetInfo | null>(null);
   const ref = useRef(null);
   const nativeAsset = allAssets.find(
     (_asset) =>
@@ -45,6 +45,16 @@ export const DestinationTokenSelector = ({
       _asset.is_gas_token
   );
   console.log("squid assets", squidAssets);
+
+  useEffect(() => {
+    if (asset) {
+      setShouldUnwrapAsset(false);
+      setSelectedAssetSymbol(
+        asset.chain_aliases[destChain.chainName.toLowerCase()].assetSymbol
+      );
+      setSelectedSquid(null);
+    }
+  }, [asset]);
 
   useOnClickOutside(ref, () => {
     dropdownOpen && handleOnDropdownToggle();
@@ -59,6 +69,14 @@ export const DestinationTokenSelector = ({
     if (!assetSymbol) return;
     setSelectedAssetSymbol(assetSymbol);
     setShouldUnwrapAsset(shouldUnwrap);
+    setSelectedSquid(null);
+  };
+
+  const handleSquidSelect = async (t: AssetInfo) => {
+    console.log("t", t);
+    setShouldUnwrapAsset(false);
+    setSelectedAssetSymbol(t.assetSymbol);
+    setSelectedSquid(t);
   };
 
   // gets native or wrapped token logo based on user choice
@@ -72,6 +90,36 @@ export const DestinationTokenSelector = ({
     return (
       <div className="left-0 w-full p-2 overflow-auto rounded-lg shadow dropdown-content menu bg-neutral">
         <ul tabIndex={0} onClick={handleOnDropdownToggle}>
+          <li key={"selected_src_asset"}>
+            <button
+              onClick={() =>
+                handleSelect(
+                  false,
+                  asset?.chain_aliases[destChain.chainName.toLowerCase()]
+                    .assetSymbol
+                )
+              }
+            >
+              <Image
+                loading="eager"
+                src={`/assets/tokens/${asset?.id}.logo.svg`}
+                layout="intrinsic"
+                width={35}
+                height={35}
+                onError={(e) => {
+                  e.currentTarget.src = defaultAssetImg;
+                  e.currentTarget.srcset = defaultAssetImg;
+                }}
+                alt="asset"
+              />
+              <span>
+                {
+                  asset?.chain_aliases[destChain.chainName.toLowerCase()]
+                    .assetSymbol
+                }
+              </span>
+            </button>
+          </li>
           {destChain?.module === "evm" && selectedAssetIsWrapped && (
             <li key={"native_version"}>
               <button onClick={() => handleSelect(true, unwrappedAssetSymbol)}>
@@ -91,47 +139,12 @@ export const DestinationTokenSelector = ({
               </button>
             </li>
           )}
-          {destChain?.module === "evm" && selectedAssetIsWrapped && (
-            <li key={"wrapped_version"}>
-              <button
-                onClick={() =>
-                  handleSelect(
-                    false,
-                    asset?.chain_aliases[destChain.chainName?.toLowerCase()]
-                      ?.assetName as string
-                  )
-                }
-              >
-                <Image
-                  loading="eager"
-                  src={`/assets/tokens/${asset?.id}.logo.svg`}
-                  layout="intrinsic"
-                  width={35}
-                  height={35}
-                  onError={(e) => {
-                    e.currentTarget.src = defaultAssetImg;
-                    e.currentTarget.srcset = defaultAssetImg;
-                  }}
-                  alt="asset"
-                />
-                <span>{wrappedAssetSymbol}</span>
-              </button>
-            </li>
-          )}
           {squidAssets.map((t) => (
             <li key={`squid_token_${t.tokenAddress}`}>
-              <button
-                onClick={() =>
-                  handleSelect(
-                    false,
-                    asset?.chain_aliases[destChain.chainName?.toLowerCase()]
-                      ?.assetName as string
-                  )
-                }
-              >
+              <button onClick={() => handleSquidSelect(t)}>
                 <Image
                   loading="eager"
-                  src={`/assets/tokens/${asset?.id}.logo.svg`}
+                  src={`/assets/tokens/${t.common_key}.logo.svg`}
                   layout="intrinsic"
                   width={35}
                   height={35}
@@ -141,7 +154,7 @@ export const DestinationTokenSelector = ({
                   }}
                   alt="asset"
                 />
-                <span>via Squid: {t.assetSymbol}</span>
+                <span>{t.assetSymbol} (via Squid)</span>
               </button>
             </li>
           ))}
@@ -167,7 +180,9 @@ export const DestinationTokenSelector = ({
               <div className="flex items-center w-full space-x-2 text-lg font-medium cursor-pointer">
                 <Image
                   loading="eager"
-                  src={`/assets/tokens/${dynamicNativeTokenLogo}.logo.svg`}
+                  src={`/assets/tokens/${
+                    selectedSquid?.common_key || dynamicNativeTokenLogo
+                  }.logo.svg`}
                   layout="intrinsic"
                   width={30}
                   height={30}
