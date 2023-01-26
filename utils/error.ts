99533fcd06d38f26e815c957134be0d43124d057
Canh Trinh <canh@axelar.network>
2023-01-26 10:57:39 -0500

    chore: remove add'l param to showErrorMsgAndThrow

--- utils/error.ts
+++ utils/error.ts
@@ -1,6 +1,6 @@
 import toast from "react-hot-toast";
 
-export function showErrorMsgAndThrow(msg: string, throwMsg: boolean = false) {
+export function showErrorMsgAndThrow(msg: string) {
   toast.error(msg);
-  if (throwMsg) throw new Error(msg);
+  throw new Error(msg);
 }
