085c9568227de3224e95856992e36e61a3571693
vladwulf <agaev.vladimir@gmail.com>
2023-01-27 18:11:28 +0000

    feat: add wallet fill btn

---
+++ features/swap-states/components/wallet-fill-btn/WalletFillBtn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { KeplrFill } from "./KeplrFill";
+import { MetamaskFill } from "./MetamaskFill";
+
+export const WalletFillBtn = () => {
+  return (
+    <>
+      <MetamaskFill />
+      <KeplrFill />
+    </>
+  );
+};
