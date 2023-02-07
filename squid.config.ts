41e2c38eca6ea135dd9bef2602d57af6f208b8a8
vladwulf <agaev.vladimir@gmail.com>
2023-02-07 10:44:46 +0000

    clean

--- squid.config.ts
+++ squid.config.ts
@@ -2,7 +2,7 @@ import { NEXT_PUBLIC_SQUID_URL } from "config/constants";
 
 import { Squid } from "@0xsquid/sdk";
 
-// // instantiate the SDK
+// instantiate the SDK
 export const squid: Squid = new Squid({
   baseUrl: NEXT_PUBLIC_SQUID_URL, // for mainnet use "https://api.0xsquid.com"
 });
