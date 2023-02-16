cb9ce3d155baa7d379516235e0ba30606ed06b24
Alan Soares <alanrsoars@gmail.com>
2023-02-16 14:51:27 +1300

    chore: remove unused import

--- src/hooks/init/useInitialChainList.ts
+++ src/hooks/init/useInitialChainList.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect } from "react";
 import { useRouter } from "next/router";
 import { ChainInfo, loadAssets } from "@axelar-network/axelarjs-sdk";
 import _ from "lodash";
