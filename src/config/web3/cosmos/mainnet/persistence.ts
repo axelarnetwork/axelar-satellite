18c9a2321f71619880b367c29b938dbd01f0ac40
Canh Trinh <canh@axelar.network>
2023-04-10 21:17:26 -0400

    chore: add persistence mainnet

---
+++ src/config/web3/cosmos/mainnet/persistence.ts
@@ -0,0 +1,40 @@
+import { Bech32Address } from "@keplr-wallet/cosmos";
+
+import { COSMOS_PROXY_RPC_MAINNET } from "../../../constants";
+import { CosmosChain } from "../interface";
+
+export const persistence: CosmosChain = {
+  chainId: "core-1",
+  chainName: "Persistence",
+  chainIdentifier: "persistence",
+  rpc: `${COSMOS_PROXY_RPC_MAINNET}/chain/persistence`,
+  rest: "https://persistence-api.polkachu.com",
+  bip44: {
+    coinType: 118,
+  },
+  bech32Config: Bech32Address.defaultBech32Config("persistence"),
+  currencies: [
+    {
+      coinDenom: "XPRT",
+      coinMinimalDenom: "uxprt",
+      coinDecimals: 6,
+      coinGeckoId: "persistence",
+    },
+  ],
+  feeCurrencies: [
+    {
+      coinDenom: "XPRT",
+      coinMinimalDenom: "uxprt",
+      coinDecimals: 6,
+      coinGeckoId: "persistence",
+    },
+  ],
+  stakeCurrency: {
+    coinDenom: "XPRT",
+    coinMinimalDenom: "uxprt",
+    coinDecimals: 6,
+    coinGeckoId: "persistence",
+  },
+  coinType: 118,
+  chainToAxelarChannelId: "channel-51",
+} as CosmosChain;
