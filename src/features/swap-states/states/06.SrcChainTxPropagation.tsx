63e78fc7fadf4ce4dec89be118179857e9c425a4
Canh Trinh <canhtrinh@users.noreply.github.com>
2023-04-11 21:01:05 -0400

    Feat/filecoin mainnet (#302)
    * chore: adding configs for filecoin mainnet
    
    * chore: hide progress bar for filecoin
    
    * Update src/features/swap-states/states/06.SrcChainTxPropagation.tsx
    
    Co-authored-by: Milap Sheth <milap@axelar.network>
    
    * chore: adding backup RPCs
    
    * chore: code cleanup
    
    * chore: add filecoin warning to evm modal popup
    
    ---------
    
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- src/features/swap-states/states/06.SrcChainTxPropagation.tsx
+++ src/features/swap-states/states/06.SrcChainTxPropagation.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import clsx from "clsx";
 import { SpinnerRoundFilled } from "spinners-react";
 import { useWaitForTransaction } from "wagmi";
 
@@ -11,14 +12,34 @@ import { Hash } from "~/types";
 import { SwapStatus } from "~/utils/enums";
 
 import { ProgressBar } from "../components";
+import { AxelarscanLink } from "../components/tx-summary-stats/AxelarscanLink";
+
+const InfoIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    className="w-5 h-5 pb-1 mx-1 stroke-current"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
 
 export const SrcChainTxPropagation = () => {
   const swapStatus = useSwapStore((state) => state.swapStatus);
-
-  const [numConfirmationsSoFar, setNumConfirmationsSoFar] = useState(1);
   const srcChain = useSwapStore((state) => state.srcChain);
   const txInfo = useSwapStore((state) => state.txInfo);
   const srcChainId = useSwapStore(getSrcChainId);
+  const [numConfirmationsSoFar, setNumConfirmationsSoFar] = useState(1);
+
+  useEffect(() => {
+    if (swapStatus === SwapStatus.IDLE) setNumConfirmationsSoFar(1);
+  }, [swapStatus]);
 
   useWaitForTransaction({
     chainId: srcChainId,
@@ -35,6 +56,39 @@ export const SrcChainTxPropagation = () => {
     ),
     enabled: !!txInfo?.sourceTxHash,
   });
+
+  const filecoinMsg = useMemo(() => {
+    return srcChain?.chainName.toLowerCase().includes("filecoin")
+      ? "Transfers from Filecoin will take at least ~8 hours to complete due to the long block finality of Filecoin, so Check Axelarscan for updates on your transfer."
+      : null;
+  }, [srcChain]);
+
+  const srcChainName = useMemo(() => {
+    if (filecoinMsg)
+      return (
+        <a
+          className="flex flex-row ml-1 cursor-pointer tooltip tooltip-warning link link-primary"
+          data-tip={filecoinMsg}
+          href={
+            "https://docs.filecoin.io/basics/what-is-filecoin/blockchain/#finality"
+          }
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          {" "}
+          {srcChain.chainName}
+          {InfoIcon}
+        </a>
+      );
+
+    let name = "";
+    if (srcChain.chainName.toLowerCase()?.includes("base")) {
+      name = "Goerli L1";
+    } else {
+      name = srcChain.chainName;
+    }
+    return <div className="ml-1"> {name}</div>;
+  }, [srcChain, filecoinMsg]);
 
   if (swapStatus !== SwapStatus.WAIT_FOR_SRC_TX_PROPAGATION) {
     return null;
@@ -93,28 +147,33 @@ export const SrcChainTxPropagation = () => {
                 />
                 <div className="flex flex-col text-center">
                   {" "}
-                  <div className="text-sm">
-                    Waiting for transaction to be finalized on{" "}
-                    {srcChain.chainName.toLowerCase()?.includes("base")
-                      ? "Goerli L1"
-                      : srcChain.chainName}
+                  <div className={clsx("flex flex-row text-sm")}>
+                    Waiting for transaction to be finalized on {srcChainName}
                   </div>
-                  <div className="text-sm text-gray-400">
-                    (
-                    {Math.min(
-                      numConfirmationsSoFar,
-                      srcChain.confirmLevel as number
-                    )}
-                    /{srcChain.confirmLevel} confirmations)
-                  </div>
+                  {!filecoinMsg && (
+                    <div className="text-sm text-gray-400">
+                      (
+                      {Math.min(
+                        numConfirmationsSoFar,
+                        srcChain.confirmLevel as number
+                      )}
+                      /{srcChain.confirmLevel} confirmations)
+                    </div>
+                  )}
                 </div>
               </div>
-              <div className="flex items-center justify-center mt-2 gap-x-2">
-                <progress
-                  className="w-56 progress progress-success"
-                  value={numConfirmationsSoFar}
-                  max={srcChain.confirmLevel}
-                />
+              {!filecoinMsg && (
+                <div className="flex items-center justify-center mt-2 gap-x-2">
+                  <progress
+                    className="w-56 progress progress-success"
+                    value={numConfirmationsSoFar}
+                    max={srcChain.confirmLevel}
+                  />
+                </div>
+              )}
+              <div className="my-0 divider" />
+              <div className="flex justify-center w-full">
+                <AxelarscanLink />
               </div>
             </div>
           </div>
