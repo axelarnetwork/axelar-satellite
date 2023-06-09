0005f6e0d6ce2f807e50a8294b57f58b2ab766e8
Canh Trinh <canh@axelar.network>
2023-06-09 18:12:51 -0400

    chore: update optimism rpc

--- src/config/web3/evm/mainnet/optimism.ts
+++ src/config/web3/evm/mainnet/optimism.ts
@@ -4,5 +4,19 @@ import { ChainExtension } from "../interface";
 
 export const optimism: ChainExtension = {
   ...optimismConfig,
+  rpcUrls: {
+    default: {
+      http: [
+        "https://optimism-mainnet.public.blastapi.io",
+        "https://optimism.publicnode.com",
+      ],
+    },
+    public: {
+      http: [
+        "https://optimism-mainnet.public.blastapi.io",
+        "https://optimism.publicnode.com",
+      ],
+    },
+  },
   networkNameOverride: "optimism",
 };
