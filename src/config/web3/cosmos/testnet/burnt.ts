f18a08ada302d557f6a5e4bba620066abb5145ae
Canh Trinh <canh@axelar.network>
2023-09-20 17:31:50 -0400

    chore: hard-coding burnt rpc

--- src/config/web3/cosmos/testnet/burnt.ts
+++ src/config/web3/cosmos/testnet/burnt.ts
@@ -1,11 +1,9 @@
 import { Bech32Address } from "@keplr-wallet/cosmos";
 
-import { COSMOS_PROXY_RPC_TESTNET } from "~/config/constants";
-
 import { CosmosChain } from "../interface";
 
 export const burnt: CosmosChain = {
-  rpc: `${COSMOS_PROXY_RPC_TESTNET}/chain/burnt`,
+  rpc: `https://rpc.xion-testnet-1.burnt.com:443`,
   rest: "https://api.xion-testnet-1.burnt.com",
   chainId: "xion-testnet-1",
   chainName: "Burnt Testnet",
@@ -18,13 +16,6 @@ export const burnt: CosmosChain = {
       coinMinimalDenom: "uxion",
       coinDecimals: 6,
       coinGeckoId: "xion",
-    },
-    {
-      coinDenom: "AXL",
-      coinMinimalDenom:
-        "ibc/D934516FBE457F3A98AFABD87E0EFF7F95A15325C191EA8CDD7763C702FDDEC2",
-      coinDecimals: 6,
-      coinGeckoId: "axelar",
     },
   ],
   feeCurrencies: [
