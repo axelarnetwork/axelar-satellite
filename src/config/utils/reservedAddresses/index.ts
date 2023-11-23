7467239f9933ce873c7d38633592eeb5dc26010d
alanrsoares <alan@axelar.network>
2023-11-23 20:42:18 -0300

    chore: remove comments

--- src/config/utils/reservedAddresses/index.ts
+++ src/config/utils/reservedAddresses/index.ts
@@ -4,8 +4,11 @@ import list1 from "./10202023";
 import list2 from "./11232023";
 import originalList from "./originalList";
 
-const additionalList = [list1, list2].flatMap(toLower);
-
-const normalized = [...originalList, ...additionalList];
+const normalized = [
+  ...originalList,
+  // additional reserved address lists
+  ...list1,
+  ...list2,
+].map(toLower);
 
 export default JSON.stringify(normalized);
