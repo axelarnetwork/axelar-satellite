cc72399996b5b3e5173896af711c5d9c67e5b602
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-09-21 11:26:44 -0400

    chore: updating reserved addresses (#484)

--- src/config/utils/reservedAddresses/index.ts
+++ src/config/utils/reservedAddresses/index.ts
@@ -2,6 +2,7 @@ import { toLower } from "rambda";
 
 import list1 from "./10202023";
 import list2 from "./11232023";
+import list3 from "./20240921";
 import originalList from "./originalList";
 
 const normalized = [
@@ -9,6 +10,7 @@ const normalized = [
   // additional reserved address lists
   ...list1,
   ...list2,
+  ...list3,
 ].map(toLower);
 
 export default JSON.stringify(normalized);
