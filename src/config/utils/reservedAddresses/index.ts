1d4da3cd6ade57567f5f27cde1995d7c38321db0
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2024-11-18 03:32:45 +1300

    chore: update reserved addresses (#485)

--- src/config/utils/reservedAddresses/index.ts
+++ src/config/utils/reservedAddresses/index.ts
@@ -3,6 +3,7 @@ import { toLower } from "rambda";
 import list1 from "./10202023";
 import list2 from "./11232023";
 import list3 from "./20240921";
+import list4 from "./20241117";
 import originalList from "./originalList";
 
 const normalized = [
@@ -11,6 +12,7 @@ const normalized = [
   ...list1,
   ...list2,
   ...list3,
+  ...list4,
 ].map(toLower);
 
 export default JSON.stringify(normalized);
