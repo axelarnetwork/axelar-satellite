8d2892a312233d880f8573a4f7263dc3e2293560
Ilya Lukyanov <ilya.lukyanov@gmail.com>
2024-11-17 22:55:58 +1300

    include file

---
+++ src/config/utils/reservedAddresses/20241117.ts
@@ -0,0 +1,5 @@
+const evmAddresses = [
+  "0x39fde96298720a689b0c95bfd3a69f38b85032d9",
+];
+
+export default evmAddresses;
--- src/config/utils/reservedAddresses/index.ts
+++ src/config/utils/reservedAddresses/index.ts
@@ -3,7 +3,7 @@ import { toLower } from "rambda";
 import list1 from "./10202023";
 import list2 from "./11232023";
 import list3 from "./20240921";
-import list4 from "./20241017";
+import list4 from "./20241117";
 import originalList from "./originalList";
 
 const normalized = [
