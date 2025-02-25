331096af152f5ab8124abc51dad999b2f619d55a
npty <nptytn2@gmail.com>
2025-02-25 10:53:58 +0700

    feat: add Bybit hacker address list

--- src/config/utils/reservedAddresses/index.ts
+++ src/config/utils/reservedAddresses/index.ts
@@ -4,6 +4,7 @@ import list1 from "./10202023";
 import list2 from "./11232023";
 import list3 from "./20240921";
 import list4 from "./20241117";
+import list5 from "./20250225";
 import originalList from "./originalList";
 
 const normalized = [
@@ -13,6 +14,7 @@ const normalized = [
   ...list2,
   ...list3,
   ...list4,
+  ...list5,
 ].map(toLower);
 
 export default JSON.stringify(normalized);
